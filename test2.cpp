dbf74382ad2ca06d27118ee7345e22cef496042d
YukiTanaka <anpanma6@gmail.com>
2026-03-29 19:17:58 +0900

    make class pointer and object

--- test2.cpp
+++ test2.cpp
@@ -10,10 +10,14 @@ class Student {
         void show(); // 学籍番号と学年を表示する関数
 };
 
+
 void Student::show(){
     cout << "学籍番号: " << num << endl;
     cout << "学年: " << year << endl;
 }
+
+// プロトタイプ宣言
+void change(Student *s);
 
 // メイン関数
 int main(){
@@ -24,5 +28,14 @@ int main(){
     Student *ptr = &tanaka;
     ptr->show(); // ポインタを使ってshow関数を呼び出す
 
+    change(ptr); // ポインタを使ってchange関数を呼び出す
+
+    ptr->show(); // 変更後の内容を表示
+
     return 0;
+}
+
+void change(Student *s){
+    s->num = 54321; // 学籍番号を変更
+    s->year = 3; // 学年を変更
 }
