af536be28886a4ae004bd25b00fddd1c6b6e07d2
YukiTanaka <anpanma6@gmail.com>
2026-03-29 18:33:12 +0900

    make struct

--- test2.cpp
+++ test2.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 using namespace std;
 
-struct Fruits{
+// 構造体定義
+typedef struct {
+    int num;
+    int year;
     char name[128];
-    int price;
-    int zaiko;
-};
+}Student;
 
 // メイン関数
 int main(){
-    // ポインタについて
-    int a = 10;
-    int *b = &a; // aのアドレスをbに格納
-    *b = 11; // bが指すアドレスの値を11に変更
-    cout << "Value of a: " << a << "\n"; // aの値
-    printf("%p\n", b); // aのアドレス
+    Student s1 = {1, 2024, "Alice"};
+    Student s2 = {2, 2023, "Bob"};
 
-    // ポインタと構造体
-    struct  Fruits store = {"Apple", 100, 50};
-    struct Fruits *p = &store; // storeのアドレスをpに格納
-    cout << "Fruit name: " << (*p).name << "\n"; // ポインタを使用して構造体のメンバにアクセス(データ型を同じにする必要がある)
+    cout << "Student 1: " << s1.num << ", " << s1.year << ", " << s1.name << endl;
+    cout << "Student 2: " << s2.num << ", " << s2.year << ", " << s2.name << endl;
 
-
+    return 0;
 }
