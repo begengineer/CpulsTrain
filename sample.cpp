59d02da9ef647f81babe5a1c0fdbb7ee10e34467
YukiTanaka <anpanma6@gmail.com>
2026-03-29 19:00:35 +0900

    make class

--- sample.cpp
+++ sample.cpp
@@ -2,8 +2,11 @@
 #include "sample.h"
 using namespace std;
 
+void Student::show(){
+    cout << "学籍番号: " << num << endl;
+    cout << "学年: " << year << endl;
+}
 
-// メイン関数
 int main(){
     Student Tanaka;
     Tanaka.num = 12345;
---
+++ sample.h
@@ -0,0 +1,6 @@
+class Student{
+    public:
+    int num; // 学籍番号
+    int year; // 学年
+    void show(); // 学籍番号と学年を表示する関数
+};
