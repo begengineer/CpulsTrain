d73946b1d06eeba79502656cb036a11fb509bc41
YukiTanaka <anpanma6@gmail.com>
2026-03-30 20:26:41 +0900

    make class capsule private

--- capsule/capsule.cpp
+++ capsule/capsule.cpp
@@ -6,11 +6,24 @@ using namespace std;
 
 class Student {
     public: // ユーザーが使用できる機能を提供
-     int keisan();
-    private: // データを隠す
+     void setNum(int n);
+     void printNmu();
+    private: // データを隠す(クラス内だけでしか値の操作ができない)
      int num;
 };
 
-int main(){
+void Student::setNum(int n){
+    num = n;
+}
 
+void Student::printNmu(){
+    cout << num << endl;
+}
+
+int main(){
+    Student s;
+    s.setNum(10);
+
+    s.printNmu();
+    return 0;
 }
