3fc2dc0d80803ae87bda42b26b34d4f5c1b4e976
YukiTanaka <anpanma6@gmail.com>
2026-03-30 21:01:09 +0900

    make class constructor overload

--- constructor/constructor.cpp
+++ constructor/constructor.cpp
@@ -4,10 +4,14 @@ using namespace std;
 // constructorについて
 // 初期値を代入するために使用する
 
+// コンストラクタのオーバーロード:関数名が同じでもデータ型が異なればエラーが出ない
+
 class NewClass{
     public:
     int num;
+    // 関数のオーバーロード
     NewClass(int a); // オブジェクト生成のタイミングで起動
+    NewClass(); // デフォルトコンストラクタ
 };
 
 // これは初期化を行うための関数(オブジェクト生成タイミングで実行される)
@@ -17,8 +21,12 @@ NewClass::NewClass(int a){
     cout << "constructor" << endl;
 }
 
+NewClass::NewClass(){
+    num = 0;
+    cout << "default constructor" << endl;
+}
+
 int main(){
-    NewClass nc(5);
-    cout << nc.num << endl;
+    NewClass nc;
     return 0;
 }
