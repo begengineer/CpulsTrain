a322622a2cce4d35714bddfa52f36da53ad3dee4
YukiTanaka <anpanma6@gmail.com>
2026-04-05 22:47:34 +0900

    bit train ver2

--- bitTrain/cBit.cpp
+++ bitTrain/cBit.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
+#include <cstring>
+#include <cstdio>
 using namespace std;
+void my_memcpy(char* dest, const char* src, int n);
+void memcpy(char* dest, const char* src, int n);
 
-int main(){
-    int num = 2;
+#define BUF_SIZE 16
 
-    num = num & ~(1 << 1);
+int main()
+{
+    // 変数定義
+    char buf1[BUF_SIZE];
+    char buf2[BUF_SIZE];
 
-    cout << num << endl;
+    // 変数初期化
+    memset(buf1, '\0', BUF_SIZE);
+    memset(buf2, '\0', BUF_SIZE);
+
+    // メモリ領域を特定の文字で埋める
+    memset(buf1, 'b', 4);  // buf1の最初の4バイトをbにする
+    memset(buf1, 'a', 2);  // buf1の最初の2バイトをaにする
+
+    // 配列の内容を表示
+    printf("配列の要素を表示(コピー前)。\n");
+    printf("buf1:%s\n", buf1);
+    printf("buf2:%s\n", buf2);
+
+    // メモリをコピーする
+    my_memcpy(buf2, buf1, 3);  // buf1の先頭3バイトをbuf2にコピーする
+
+    // 配列の内容を表示
+    printf("配列の要素を表示(コピー後)。\n");
+    printf("buf1:%s\n", buf1);
+    printf("buf2:%s\n", buf2);
+
     return 0;
+}
+void my_memcpy(char* dest, const char* src, int n){
+    for(int i = 0; i <= n; i++){
+        dest[i] = src[i];
+    }
 }
